c75bab6cee6325d28255c66c21a60720c8185c10
Anni1123 <kumarianni1213@gmail.com>
2020-02-29 06:59:49 +0530

    Second Commit

--- app/src/main/java/com/example/newproject/Workoutexpert.java
+++ app/src/main/java/com/example/newproject/Workoutexpert.java
@@ -2,12 +2,15 @@ package com.example.newproject;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+
+
 public class Workoutexpert {
     List<String> getWorkouts(String workouttypes){
         List<String> workout=new ArrayList<String>();
         if(workouttypes.equals("Chest")){
         workout.add("Bench Press");
         workout.add("Cable Fiys");
+
         }
         else if (workouttypes.equals("Triceps")){
             workout.add("Tricep Ext");
@@ -19,6 +22,7 @@ public class Workoutexpert {
         }
         else if(workouttypes.equals("Biceps")){
             workout.add("Biceps Curis");
+            workout.add("Cable Fiys");
         }
         return workout;
     }
